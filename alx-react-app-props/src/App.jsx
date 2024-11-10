712b28405cba94c5c3372f14db2860127beb967d
Nhlamuloj <nmahlabana@gmail.com>
2024-11-10 08:38:19 -0800

    updated

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import UserContext from './UserContext.js/UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
