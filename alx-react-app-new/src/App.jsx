a4e148388e3858ee4a4562883a92b47a371163a8
Nhlamuloj <nmahlabana@gmail.com>
2024-11-10 09:58:07 -0800

    updated folder

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Header from './components/Header'
 import UserProfile from './components/UserProfile'
 import Counter from './components/Counter'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'
 
 function App() {
   
@@ -12,6 +14,8 @@ function App() {
       <Header />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Counter />
+      <MainContent />
+      <Footer />
     </div>
   )
 }
