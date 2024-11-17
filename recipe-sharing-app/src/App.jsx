50dc2bd1f6c35099cbd43acf31fb9c3cd75a3715
Nhlamuloj <nmahlabana@gmail.com>
2024-11-17 16:25:16 +0200

    recipe-Sharing-app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,16 @@
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeList from './RecipeList'; // A component displaying all recipes
+import RecipeDetails from './RecipeDetails';
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Manager</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   );
 };
 
