9de6053e4f6289c9fd6c8f140de66cb8f87070c0
Nhlamuloj <nmahlabana@gmail.com>
2024-11-17 16:55:29 +0200

    recipe-Sharing-app

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,11 +1,19 @@
 // RecipeDetails component
+import { useRecipeStore } from './recipeStore';
 import RecipeDetails from './components/RecipeDetails';
 
-export const RecipeDetails = () => {
-    return (
-      <div>
-        <h1>Recipe Details</h1>
-      </div>
-    );
-  };
+
+const RecipeDetails = ({ recipeId }) => {
+  const recipe = useRecipeStore(state =>
+    state.recipes.find(recipe => recipe.id === recipeId)
+  );
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      {/* Render EditRecipeForm and DeleteRecipeButton here */}
+    </div>
+  );
+};
   export default RecipeDetails;
