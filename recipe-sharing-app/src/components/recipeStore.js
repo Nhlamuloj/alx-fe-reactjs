0dcb87c68aa716b37074ec284c1383338d8ade2b
Nhlamuloj <nmahlabana@gmail.com>
2024-11-17 16:32:04 +0200

    recipe-Sharing-app

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,7 +1,21 @@
 import create from 'zustand';
+import { updateRecipe, deleteRecipe } from '../recipeStore';
+import updateRecipe from '../recipeStore';
+
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
 }));
+
+export const updateRecipe = (id, updatedData) => {
+  console.log(`Updating recipe with ID: ${id}`);
+  // Update logic here
+};
+
+export const deleteRecipe = (id) => {
+  console.log(`Deleting recipe with ID: ${id}`);
+  // Delete logic here
+};
+
