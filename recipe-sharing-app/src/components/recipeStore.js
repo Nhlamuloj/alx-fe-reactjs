aeccf1bf0818e566f991b232e69b864e4fb20499
Nhlamuloj <99670642+Nhlamuloj@users.noreply.github.com>
2024-11-18 09:57:34 +0200

    updated

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,6 +1,5 @@
 
 import create from 'zustand';
-import  { createContext, useState, useContext } from 'react';
 
 const useRecipeStore = create(set => ({
   recipes: [], 
@@ -20,24 +19,4 @@ const useRecipeStore = create(set => ({
     return { recommendations: recommended };
   }),
 }));
-
-
-// Create the context
-const RecipeContext = createContext();
-
-// Provider component
-export const RecipeProvider = ({ children }) => {
-  const [searchTerm, setSearchTerm] = useState('');
-
-  return (
-    <RecipeContext.Provider value={{ searchTerm, setSearchTerm }}>
-      {children}
-    </RecipeContext.Provider>
-  );
-};
-
-// Custom hook for consuming the context
-export const useRecipeStores = () => {
-  return useContext(RecipeContext);
-};
 
