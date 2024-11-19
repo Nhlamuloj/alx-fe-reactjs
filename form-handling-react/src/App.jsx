a0dde086e41defaa0a7c80baaef16a42172dbff6
Nhlamuloj <nmahlabana@gmail.com>
2024-11-19 22:25:20 +0200

    new up date

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,11 +1,10 @@
 import RegistrationForm from "./Components/RegistrationForm";
 import RegistrationFormWithFormik from "./Components/formikForm";
-
 const App = () => {
   return (
     <div>
       <RegistrationForm />
-      <RegistrationFormWithFormik />
+       <RegistrationFormWithFormik />
     </div>
   );
 };
