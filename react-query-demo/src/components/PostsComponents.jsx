0a55ca797edbd4ac08fa3f9f4d60fda974b2b9f7
Nhlamuloj <99670642+Nhlamuloj@users.noreply.github.com>
2024-11-25 09:59:45 +0200

    testing

---
+++ react-query-demo/src/components/PostsComponents.jsx
@@ -0,0 +1,14 @@
+// src/components/PostsComponent.jsx
+
+import React from 'react';
+
+const PostsComponent = () => {
+  return (
+    <div>
+      <h1>Posts</h1>
+      <p>This is the PostsComponent.</p>
+    </div>
+  );
+};
+
+export default PostsComponent;
