c3f593446cbe97267a5262fa746fb563119a2340
Nhlamuloj <nmahlabana@gmail.com>
2024-11-26 22:38:07 +0200

    update

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,5 +1,6 @@
 
 import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
@@ -9,11 +10,14 @@ const App = () => (
     <nav>
       <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/profile">Profile</Link>
     </nav>
+    <BrowserRouter>
+   
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/profile/*" element={<Profile />} />
     </Routes>
+    </BrowserRouter>
   </div>
 );
 
