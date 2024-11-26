4d2058b3fe6e617907accbbba1625f0c099bda04
Nhlamuloj <nmahlabana@gmail.com>
2024-11-26 22:30:50 +0200

    update

---
+++ react-router-advanced/src/pages/Profile.jsx
@@ -0,0 +1,19 @@
+
+import { Routes, Route, Link, } from "react-router-dom";
+import ProfileSettings from "../pages/Profile"
+import ProfileDetails from "../ProfileDetails";
+
+const Profile = () => (
+  <div>
+    <h1>Profile Page</h1>
+    <nav>
+      <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
+    </nav>
+    <Routes>
+      <Route path="details" element={<ProfileDetails />} />
+      <Route path="settings" element={<ProfileSettings />} />
+    </Routes>
+  </div>
+);
+
+export default Profile;
