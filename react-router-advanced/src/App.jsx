60647278f03a3bd91eaad4ff450bfe6b8aa4da53
Nhlamuloj <nmahlabana@gmail.com>
2024-11-28 21:49:12 +0200

    advanced update

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,19 +1,24 @@
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Profile from "./components/Profile";
-import Login from "./components/Login";
-import NotFound from "./components/NotFound";
+import BlogPost from "./components/BlogPost"
 import ProtectedRoute from "./components/ProtectedRoute";
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        {/* Protected Route */}
-        <Route
+const App = () => (
+  <div>
+    <nav>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/profile">Profile</Link>
+    </nav>
+    <BrowserRouter>
+   
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog/:id" element={<BlogPost />} />
+      <Route path="/about" element={<About />} />
+      <Route
           path="/profile/*"
           element={
             <ProtectedRoute>
@@ -21,10 +26,9 @@ const App = () => {
             </ProtectedRoute>
           }
         />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
-};
+    </Routes>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
