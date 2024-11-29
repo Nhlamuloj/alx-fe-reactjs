22c69b0933ec6ec8481ced126b4e742a5ed2999c
Nhlamuloj <nmahlabana@gmail.com>
2024-11-29 23:10:12 +0200

    user

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
-        className="rounded-full w-24 h-24 sm:w-36  md:max-w-sm sm:h-36 sm:p-4 mx-auto" 
+        className="rounded-full w-24 h-24 sm:w-36  md:max-w-sm sm:h-36 sm:p-4 mx-auto sm:w-24sm:h-24 md:w-36 md:h-36" 
       />
       <h1 className="text-lg sm:text-xl  md:max-w-sm text-blue-800 my-4 md:text-xl">John Doe</h1>
       <p className="text-sm sm:text-base text-gray-600 md:text-xl">
