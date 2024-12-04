0e3d4847c4d2165328e623c30dd82985611e5907
Nhlamuloj <99670642+Nhlamuloj@users.noreply.github.com>
2024-12-04 12:51:39 +0200

    updated

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 md:p-8 sm:p-4 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-8 md:p-8 sm:p-4 max-w-xs md:max-w-sm max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 md:p-8 sm:p-4 h-36 mx-auto"
+        className="rounded-full w-36 md:p-8 max-w-xs md:max-w-sm max-w-sm sm:p-4 h-36 mx-auto"
       />
-      <h1 className="text-xl text-blue-800 my-4 md:p-8 sm:p-4 text-center">John Doe</h1>
-      <p className="text-gray-600 md:p-8 sm:p-4 text-base text-center">
+      <h1 className="text-xl text-blue-800 my-4  max-w-xs md:max-w-sm max-w-sm md:p-8 sm:p-4 text-center">John Doe</h1>
+      <p className="text-gray-600 md:p-8 sm:p-4 max-w-xs md:max-w-sm max-w-sm text-base text-center">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
