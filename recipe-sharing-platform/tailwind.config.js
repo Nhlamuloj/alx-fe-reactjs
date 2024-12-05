7ce9212ec38d0acf9491710003713c8dbcb16b2c
Nhlamuloj <nmahlabana@gmail.com>
2024-12-05 22:17:17 +0200

    updated

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+ module.exports = {
+  content: [
+    "./public/index.html", // Ensure Tailwind processes the index.html file
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all React component files in src
+  ],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
