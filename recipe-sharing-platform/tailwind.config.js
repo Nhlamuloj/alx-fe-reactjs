456b6b007b31cb653bd988075cb81fe2cd2a696f
Nhlamuloj <nmahlabana@gmail.com>
2024-12-08 09:00:20 +0200

    updated jason

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -3,7 +3,7 @@
     "./public/index.html", // Ensure Tailwind processes the index.html file
     "./src/**/*.{js,jsx,ts,tsx}", // Include all React component files in src
   ],
-  darkMode: false, // You can set it to 'media' or 'class' if needed
+  darkMode: false, 
   theme: {
     extend: {},
   },
