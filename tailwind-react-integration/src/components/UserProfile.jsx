0f5426532960c850705eba7cecb28c4205c5de27
Nhlamuloj <nmahlabana@gmail.com>
2025-01-03 15:46:51 -0800

    updated profile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 const UserProfile = () => {
   return (
-    <div className="bg-white p-6 rounded-lg  shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+    <div className="bg-white p-6 rounded-lg  shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out  max-w-xs md:max-w-sm  text-lg  md:text-xl text-sm">
       <div className="flex items-center space-x-4">
        <img
          src="profile-image.jpg"
