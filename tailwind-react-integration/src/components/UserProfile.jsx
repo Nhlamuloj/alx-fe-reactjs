37100ff5a6a8f3366c6b787f812d29222562ccc4
Nhlamuloj <nmahlabana@gmail.com>
2025-01-03 15:28:07 -0800

    updated profile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,18 +1,19 @@
 const UserProfile = () => {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+    <div className="bg-white p-6 rounded-lg  shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <div className="flex items-center space-x-4">
-      <img
-  src="profile-image.jpg"
-  alt="Profile Image"
-  class="w-32 h-32 rounded-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"/>
+       <img
+         src="profile-image.jpg"
+          alt="Profile Image"
+         class="w-32 h-32 rounded-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+        />
 
         <div>
-          <h1 className="text-2xl font-bold hover:text-blue-500">John Doe</h1>
-          <p className="text-gray-600">Software Developer</p>
+          <h1 className="text-2xl font-bold hover:text-blue-500 bg-gray-100 p-8 max-w-sm mx-auto my-20">John Doe</h1>
+          <p className="text-gray-600 bg-gray-100 p-8 max-w-sm mx-auto my-20">Software Developer</p>
         </div>
       </div>
-      <p className="mt-4 text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p className="mt-4 text-gray-500 bg-gray-100 p-8 max-w-sm mx-auto my-20" >Lorem  nhlamulo ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
   );
 };
